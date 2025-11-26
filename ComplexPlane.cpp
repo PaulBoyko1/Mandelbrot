3d0098b58bb04d4861115a43c2c6b27ef9ead0b9
PaulBoyko1 <Paulboyko20@gmail.com>
2025-11-26 10:17:35 -0800

    Refactor ComplexPlane drawing and zoom functionality
    Updated drawing method to include render states and modified zoom functions to adjust plane size correctly. Enhanced text loading and iteration counting for better rendering of Mandelbrot set.

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -15,7 +15,7 @@ ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight) {
 }
 void ComplexPlane::draw(RenderTarget& target, RenderStates states) const {
 
-    target.draw(m_vArray);
+    target.draw(m_vArray, states);
 
 }
 void ComplexPlane::updateRender() {
@@ -38,40 +38,92 @@ void ComplexPlane::updateRender() {
 void ComplexPlane::zoomIn() {
 
     m_zoomCount++;
+    double x = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomCount));
+    double y = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_zoomCount));
+    m_plane_size = Vector2f(x, y);
+    m_state = State::CALCULATING;
 
 }
 void ComplexPlane::zoomOut() {
 
-
+    m_zoomCount -= 1;
+    double x = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomCount));
+    double y = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_zoomCount));
+    m_plane_size = Vector2f(x, y);
+    m_state = State::CALCULATING;
 
 }
 void ComplexPlane::setCenter(Vector2i mousePixel) {
-
-
-    
+    m_plane_center = mapPixelToCoords(mousePixel);
+    m_state = State::CALCULATING;
 }
 void ComplexPlane::setMouseLocation(Vector2i mousPixel) {
-
-
-
+     m_mouseLocation = mapPixelToCoords(pixel);
 }
 void ComplexPlane::loadText(Text& text) {
-
-
-
+    stringstream ss;
+    ss << "Mandelbrot Set" << endl;
+    ss << "Center: (" << m_plane_center.x << ", " << m_plane_center.y << ")" << endl;
+    ss << "Cursor: (" << m_mouseLocation.x << ", " << m_mouseLocation.y << ")" << endl;
+    ss << "Left-click to Zoom in" << endl;
+    ss << "Right-click to Zoom out" << endl;
+    text.setString(ss.str());
 }
 size_t ComplexPlane::countIterations(Vector2f coord) {
+    complex<float> c(coord.x, coord.y);
+    complex<float> z(0, 0);
 
+    int iter = 0;
 
+    while (abs(z) <= 2.0f && iter < MAX_ITER)
+    {
+        z = z * z + c;
+        iter++;
+    }
 
+    return iter;
 }
 void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b) {
+    if (count >= MAX_ITER) 
+    { 
+        r = g = b = 0; 
+        return; 
+    }
 
+    float n = (float)count / MAX_ITER;
 
-
+    if (n < 0.2) {
+        r = 128;
+        g = 0;
+        b = 128;
+    }
+    else if (n < 0.4) {
+        r = 64;
+        g = 224;
+        b = 208;
+    }
+    else if (n < 0.6) {
+        r = 0;
+        g = 225;
+        b = 0;
+    }
+    else if (n < 0.8f) {
+        r = 255;
+        g = 255;
+        b = 0;
+    }
+    else {
+        r = 255;
+        g = 0;
+        b = 0;
+    }
 }
 Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel) {
+    float left = m_plane_center.x - m_plane_size.x / 2.f;
+    float top  = m_plane_center.y - m_plane_size.y / 2.f;
 
+    float x = ((float)mousePixel.x / m_pixel_size.x) * m_plane_size.x + left;
+    float y = ((float)(m_pixel_size.y - mousePixel.y) / m_pixel_size.y) * m_plane_size.y + top;
 
-
+    return Vector2f(x, y);
 }
